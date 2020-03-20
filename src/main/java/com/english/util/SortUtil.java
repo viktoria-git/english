46425fcf6d170fce8ae6251c745d2aedb567db3f
viktoria-git <viktoriaobuhova74@gmail.com>
2020-03-20 12:24:30 +0200

    refactoring SortUtil

--- src/main/java/com/english/util/SortUtil.java
+++ src/main/java/com/english/util/SortUtil.java
@@ -8,39 +8,28 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class SortUtil {
-    public static List<WordResponse> sortByWord(List<WordResponse> wordResponses) {
-        return sort(wordResponses, WordResponse::getWord);
-    }
-
-    public static List<WordResponse> sortByTranslate(List<WordResponse> wordResponses) {
-        return sort(wordResponses, WordResponse::getTranslate);
-    }
-
-    public static List<WordResponse> sortByTopic(List<WordResponse> wordResponses) {
-        return sort(wordResponses, WordResponse::getTopicName);
-    }
-
-    public static List<WordResponse> sortByLevel(List<WordResponse> wordResponses) {
-        return wordResponses.stream()
-                .sorted(Comparator.comparing(WordResponse::getLevelId))
-                .collect(Collectors.toList());
-    }
 
     public static List<WordResponse> dispatchSort(List<WordResponse> wordResponses, String sort) {
         switch (sort) {
             case "word":
-                return sortByWord(wordResponses);
+                return sort(wordResponses, WordResponse::getWord);
             case "translate":
-                return sortByTranslate(wordResponses);
+                return sort(wordResponses, WordResponse::getTranslate);
             case "topic":
-                return sortByTopic(wordResponses);
+                return sort(wordResponses, WordResponse::getTopicName);
             case "level":
-                return sortByLevel(wordResponses);
+                return sortByLevel(wordResponses, WordResponse::getId);
         }
         return wordResponses;
     }
 
     private static List<WordResponse> sort(List<WordResponse> wordResponses, Function<WordResponse, String> func) {
+        return wordResponses.stream()
+                .sorted(Comparator.comparing(func))
+                .collect(Collectors.toList());
+    }
+
+    private static List<WordResponse> sortByLevel(List<WordResponse> wordResponses, Function<WordResponse,Integer> func) {
         return wordResponses.stream()
                 .sorted(Comparator.comparing(func))
                 .collect(Collectors.toList());
