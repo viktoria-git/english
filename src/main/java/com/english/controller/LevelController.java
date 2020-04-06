7d060f3e7335e66fb8b3ebf950585394e49e681f
vobukhova <viktoriaobuhova74@gmail.com>
2020-04-06 13:48:32 +0300

    en-6: add logger to level and topic controllers

--- src/main/java/com/english/controller/LevelController.java
+++ src/main/java/com/english/controller/LevelController.java
@@ -2,9 +2,10 @@ package com.english.controller;
 
 import com.english.entity.Level;
 import com.english.service.LevelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 public class LevelController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LevelController.class);
     private final LevelService levelService;
 
     @Autowired
@@ -19,8 +21,9 @@ public class LevelController {
         this.levelService = levelService;
     }
 
-    @RequestMapping(path = "/levels", method = RequestMethod.GET)
+    @GetMapping(path = "/levels")
     public List<Level> getAll() {
+        LOGGER.info("Get all levels");
         return levelService.getAll();
     }
 }
--- src/main/java/com/english/controller/TopicController.java
+++ src/main/java/com/english/controller/TopicController.java
@@ -2,9 +2,10 @@ package com.english.controller;
 
 import com.english.entity.Topic;
 import com.english.service.TopicService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TopicController.class);
     private final TopicService topicService;
 
     @Autowired
@@ -19,8 +21,9 @@ public class TopicController {
         this.topicService = topicService;
     }
 
-    @RequestMapping(path = "/topics", method = RequestMethod.GET)
+    @GetMapping(path = "/topics")
     public List<Topic> getAll() {
+        LOGGER.info("Get all topics");
         return topicService.getAll();
     }
 }
